5d3f908b4301f3c3b50b8febc3e3077f12a34cce
kenpang <495365458@qq.com>
2016-02-27 10:09:24 +0800

    新建网易新闻

---
+++ NeteaseNews/NeteaseNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NeteaseNews
+//
+//  Created by Pangkun on 16/2/27.
+//  Copyright © 2016年 Pangkun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NeteaseNews/NeteaseNews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NeteaseNews
+//
+//  Created by Pangkun on 16/2/27.
+//  Copyright © 2016年 Pangkun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
